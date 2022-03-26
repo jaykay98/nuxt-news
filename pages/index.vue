dceb3ae60ce88a33e56687cdd9e0a932e033b618
Jason Kidd <jason_kidd@live.com.au>
2022-03-26 13:16:35 +1000

    Add alt text to images for better accessibility

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,43 @@
 <template>
-  <section class="section">
-    <div v-for="(article, index) in newsArticles.articles" :key="index">
-      <h3>{{ article.title }}</h3>
+  <div>
+    <p v-if="$fetchState.pending" class="has-text-centered">
+      Grabbing news articles...
+    </p>
+    <p v-else-if="$fetchState.error" class="is-danger has-text-centered">
+      Oops! Looks like something went wrong
+    </p>
+    <div
+      v-for="(article, index) in newsArticles.articles"
+      v-else
+      :key="index"
+      class="card mb-6"
+    >
+      <header class="card-header">
+        <p class="card-header-title is-centered title">{{ article.title }}</p>
+      </header>
+      <div class="card-image is-centered">
+        <img
+          :src="article.urlToImage"
+          width="100%"
+          alt="An image of the news article content"
+        />
+      </div>
+      <div class="card-content">
+        <div class="content">
+          {{ article.description }}
+        </div>
+        <div class="content">
+          <time>{{ article.publishedAt.toLocaleString() }}</time>
+
+          <footer class="card-footer">
+            <a :href="article.url" target="_blank" class="card-footer-item"
+              >Open in New Tab</a
+            >
+          </footer>
+        </div>
+      </div>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
@@ -16,7 +50,7 @@ export default {
   },
   async fetch() {
     this.newsArticles = await fetch(
-      `https://newsapi.org/v2/top-headlines?country=au&apiKey=f982225deff14ca1b46cbb4a1dbba64f`
+      `https://newsapi.org/v2/top-headlines?country=au&apiKey=${process.env.NUXT_ENV_NEWS_API_KEY}`
     ).then((res) => res.json())
   },
   head() {
