1bd947a5eccb796b4e38c7b4868c8d0281695e73
Jason Kidd <jason_kidd@live.com.au>
2022-03-27 11:13:54 +1000

    Center the content on the page

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <h1 class="title">About Us</h1>
-    <p class="subtitle">
+    <h1 class="title has-text-centered">About Us</h1>
+    <p class="subtitle has-text-centered">
       The latest Australian news brought to you by
       <a href="https://newsapi.org/" target="_blank">News API</a>
     </p>
